e10ba04c8c8f0473c8fedc71042863bd497ef7f0
Mision-Innova <misioninnova@gmail.com>
2025-02-20 17:09:23 -0500

    Update [...nextauth].js
    agggggg

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -8,17 +8,20 @@ export default NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       authorization: {
         params: {
-          scope:
-            "https://www.googleapis.com/auth/userinfo.profile " +
-            "https://www.googleapis.com/auth/userinfo.email " +
-            "https://www.googleapis.com/auth/drive.file",
+          scope: [
+            "https://www.googleapis.com/auth/userinfo.profile",
+            "https://www.googleapis.com/auth/userinfo.email",
+            "https://www.googleapis.com/auth/drive.file"
+          ].join(" "),
         },
       },
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
-    // Solución 1: Usar convención _ para variables no utilizadas
-    async redirect({ url: _url, baseUrl: _baseUrl }) {
+    // Solución 1 (recomendada para TypeScript)
+    async redirect({ url: _, baseUrl: __ }) {
       return "https://www.misioninnova.org/p/biblioteca.html";
     },
+  },
+});
